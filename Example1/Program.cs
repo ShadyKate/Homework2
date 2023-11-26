1907c57fb7e39779114341b97df6ab4cf1874a36
Kate <katehappy16@gmail.com>
2023-11-26 17:23:05 +0300

    firstExampleMadeUp

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine()!);
+if (Number % 7 == 0 && Number % 23 == 0)
+Console.WriteLine($"Число {Number} кратно 7 и 23 одновременно");
+else
+{
+    Console.WriteLine($"Число {Number} не кратно 7 и 23 одновременно");
+}
