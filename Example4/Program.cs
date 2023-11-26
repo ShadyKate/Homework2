3ffcd649f212c69d9d3b497c1e01cfd63b9bc167
Kate <katehappy16@gmail.com>
2023-11-26 19:02:44 +0300

    fourthExampleMadeUp

---
+++ Example4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое натуральное число: ");
+int Number = int.Parse(Console.ReadLine()!);
+if (Number < 10)
+Console.WriteLine(Number);
+else 
+{
+    while (Number > 0)
+    {
+        int result = Number % 10;
+        Number = Number / 10;
+        if (Number > 0)
+        {
+            Console.Write(result + ",");
+        }
+        else
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
