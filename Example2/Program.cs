cfbad5606e1e6da20145e7665d3a46207e7b7f03
Kate <katehappy16@gmail.com>
2023-11-26 17:51:05 +0300

    secondExampleMadeUp

---
+++ Example2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.WriteLine ("Введите координату X: ");
+int X = int.Parse(Console.ReadLine()!);
+Console.WriteLine ("Введите координату Y: ");
+int Y = int.Parse(Console.ReadLine()!);
+while (X == 0 || Y == 0)
+{
+    Console.WriteLine("Координаты не должны равняться нулю!\nИзмените координаты!!");
+    Console.WriteLine ("Введите координату X: ");
+    X = int.Parse(Console.ReadLine()!);
+    Console.WriteLine ("Введите координату Y: ");
+    Y = int.Parse(Console.ReadLine()!);
+}
+if (X > 0 && Y > 0)
+Console.WriteLine($"Точка с координатами ({X}; {Y}) лежит в первой координатной четверти");
+if (X < 0 && Y > 0)
+Console.WriteLine($"Точка с координатами  ({X}; {Y}) лежит во второй координатной четверти");
+if (X < 0 && Y < 0)
+Console.WriteLine($"Точка с координатами ({X}; {Y}) лежит в третьей координатной четверти");
+if (X > 0 && Y < 0)
+Console.WriteLine($"Точка с координатами ({X}; {Y}) лежит в четвёртой координатной четверти");
+
